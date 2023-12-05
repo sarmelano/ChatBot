20e5196cb06a3e895564cbb0accffcac6d061097
sarmelano <sarmelano.main@gmail.com>
2023-12-04 16:01:31 -0800

    sendMessage btn added

--- scripts/main.js
+++ scripts/main.js
@@ -1,52 +1,55 @@
-let answers = ["Привет!", "Как дела?", "Что ты делаешь?", "Понятно", "Хорошо", "До встречи!"];
-let isChatting = true;
+let answers = ["Hey!", "How are you?", "What are you doing now?", "Got it", "Ok", "See you!"];
+    let isChatting = true;
 
-function byeBye () {
-    isChatting = false;
-    document.getElementById('user-input').placeholder = 'Чат завершён';
-}
+    function byeBye() {
+      isChatting = false;
+      document.getElementById('user-input').placeholder = 'Chat end';
+      document.getElementById('send-button').disabled = true;
+    }
 
-async function botResponse(randomTime, randomAnswer) {
-    let typingIndicator = document.createElement('p');
-    typingIndicator.classList.add('bot_answer');
-    typingIndicator.textContent = 'Печатает ...';
-    document.querySelector('#chat').appendChild(typingIndicator);
+    async function botResponse(randomTime, randomAnswer) {
+      let typingIndicator = document.createElement('p');
+      typingIndicator.classList.add('bot_answer');
+      typingIndicator.textContent = 'Typing  ...';
+      document.querySelector('#chat').appendChild(typingIndicator);
 
-    await new Promise(resolve => setTimeout(resolve, randomTime));
-    if (isChatting) {
+      await new Promise(resolve => setTimeout(resolve, randomTime));
+      if (isChatting) {
         document.querySelector('#chat').removeChild(typingIndicator);
         addToChat(randomAnswer);
+      }
     }
-}
 
-function addToChat(text) {
-    let chat = document.querySelector('#chat');
-    let message = document.createElement('p');
-    message.classList.add('msgFit');
-    message.textContent = text;
-    chat.appendChild(message);
-    chat.scrollTop = chat.scrollHeight;
-}
+    function addToChat(text) {
+      let chat = document.querySelector('#chat');
+      let message = document.createElement('p');
+      message.classList.add('msgFit');
+      message.textContent = text;
+      chat.appendChild(message);
+      chat.scrollTop = chat.scrollHeight;
+    }
 
-document.querySelector('#user-input').addEventListener('keypress', function(e) {
-    if (e.key === 'Enter' && isChatting) {
-        let userText = e.target.value;
-        addToChat(userText);
-        e.target.value = '';
+    function sendMessage() {
+      let userText = document.getElementById('user-input').value;
+      addToChat(userText);
+      document.getElementById('user-input').value = '';
 
-        if (userText.toLowerCase() === 'пока') {
-            addToChat("Спасибо за беседу! До свидания!");
+      if (userText.toLowerCase() === 'bye') {
+        addToChat("Okay! See you soon!");
+        byeBye();
+      } else {
+        let randomTime = Math.floor(Math.random() * 10000) + 1000;
+        let randomAnswer = answers[Math.floor(Math.random() * answers.length)];
+        botResponse(randomTime, randomAnswer).then(() => {
+          if (randomAnswer === "See you!") {
             byeBye();
-            e.target.disabled = true; 
-        } else {
-            let randomTime = Math.floor(Math.random() * 10000) + 1000;
-            let randomAnswer = answers[Math.floor(Math.random() * answers.length)];
-            botResponse(randomTime, randomAnswer).then(() => {
-                if (randomAnswer === "До встречи!") {
-                    byeBye();
-                    e.target.disabled = true; 
-                }
-            });
-        }
+          }
+        });
+      }
     }
-});
+
+    document.getElementById('user-input').addEventListener('keypress', function(e) {
+      if (e.key === 'Enter' && isChatting) {
+        sendMessage();
+      }
+    });
